aa7f7230fb7586902c97bbd558b780428d89306a
Andoni Angulo <andoni.angulo@zylk.com>
2021-08-04 14:02:22 +0200

    Improve readability of starting utils python scripts

--- FractalMLServer-Lite/deploy/.starting_utils/nginx-project.py
+++ FractalMLServer-Lite/deploy/.starting_utils/nginx-project.py
@@ -1,19 +1,36 @@
 import sys
 
-n = int(sys.argv[1])
+"""Script for generating nginx project.conf file based on desired number of Modelhosts"""
 
-f = open('./service-configurations/nginx-config/project.conf', 'w')
-cab = '\nupstream fractalmlserver_modelhost_cluster {\n'
-f.write(cab)
-for i in range(n):
-    server = '    server 172.24.0.' + str(11 + i) + ':8000;\n'
-    f.write(server)
-f.write('}\n\n')
-cluster = 'server {\n' \
-          '    listen 80;\n' \
-          '\n' \
-          '    location / {\n' \
-          '        proxy_pass http://fractalmlserver_modelhost_cluster;\n' \
-          '    }\n' \
-          '}'
-f.write(cluster)
+file_part1 = """\
+upstream fractalmlserver_modelhost_cluster {
+"""
+
+# dynamic part
+file_part2 = """\
+    server 172.24.0.{0}:8000;
+"""
+
+file_part3 = """\
+}
+
+server {
+    listen 80;
+
+    location / {
+        proxy_pass http://fractalmlserver_modelhost_cluster;
+    }
+}
+"""
+
+with open('./service-configurations/nginx-config/project.conf', 'w') as f:
+    # write file part 1
+    f.write(file_part1)
+
+    # write for each Modelhost
+    num_modelhosts = int(sys.argv[1])
+    for i in range(num_modelhosts):
+        f.write(file_part2.format(i + 11))
+
+    # write file part 3
+    f.write(file_part3)
