a9d57fdbab779a40409a62ad0c5fed17ba946eaa
agg001 <agonzalezgil@zylk.net>
2021-09-28 09:59:33 +0200

    Images method upgraded

--- FractalMLServer-Lite/modelhost/flask_app/app.py
+++ FractalMLServer-Lite/modelhost/flask_app/app.py
@@ -155,6 +155,28 @@ def log_response(response):
 
 
 @server.route(path.join(MODELHOST_BASE_URL, 'models/<model_name>/prediction'), methods=['POST'])
+
+#
+#     inference_session = model_sessions[model_name]['inference_session']
+#     input_name = model_sessions[model_name]['input_name']
+#     output_name = model_sessions[model_name]['output_name']
+#
+#     new_observation = request.json['values']
+#     try:
+#         prediction = inference_session.run(
+#             [output_name],
+#             {input_name: [new_observation]}
+#         )[0]
+#     except Exception as error:  # Error provided by the model
+#         return Prediction(
+#             500, http_status_description=str(error)
+#         ).json()
+#
+#     # Correct prediction
+#     return Prediction(
+#         200, http_status_description='Prediction successful', values=prediction
+#     ).json()
+
 def predict(model_name):
     metric_manager.increment_model_counter()
 
@@ -171,20 +193,57 @@ def predict(model_name):
     output_name = model_sessions[model_name]['output_name']
 
     new_observation = request.json['values']
-    try:
-        prediction = inference_session.run(
-            [output_name],
-            {input_name: [new_observation]}
-        )[0]
-    except Exception as error:  # Error provided by the model
-        return Prediction(
-            500, http_status_description=str(error)
-        ).json()
+    model_dimensions = model_sessions[model_name]['dimensions'][1:]
+    image_dimensions = list(numpy.shape(new_observation))
 
-    # Correct prediction
-    return Prediction(
-        200, http_status_description='Prediction successful', values=prediction
-    ).json()
+    if model_dimensions == image_dimensions:
+        try:
+            prediction = inference_session.run(
+                [output_name],
+                {input_name: [new_observation]}
+            )[0]
+
+            return Prediction(
+                200, http_status_description='Prediction successful', values=prediction
+            ).json()
+
+            # Error provided by the model
+        except Exception as error:
+            return Prediction(
+                500, http_status_description=str(error)
+            ).json()
+
+    elif model_dimensions != image_dimensions:
+        npimage = numpy.asarray(new_observation)
+        new_observation2 = numpy.rollaxis(npimage, 2, 0).tolist()
+        image2_dimensions = list(numpy.shape(new_observation2))
+        print(image2_dimensions)
+        print(model_dimensions)
+
+        if image2_dimensions == model_dimensions:
+            try:
+                prediction = inference_session.run(
+                    [output_name],
+                    {input_name: [new_observation2]}
+                )[0]
+
+                # Error provided by the model
+            except Exception as error:
+                return Prediction(
+                    500, http_status_description=str(error)
+                ).json()
+
+                # Correct prediction
+            return Prediction(
+                200, http_status_description='Prediction successful', values=prediction
+            ).json()
+        else:
+            return Prediction(
+                404,
+                http_status_description=f'{model_name} does not support this input. {image_dimensions} is '
+                                        f'received, but {model_dimensions} is allowed. Please, check it and try '
+                                        f'again. '
+            ).json()
 
 
 @server.route(path.join(MODELHOST_BASE_URL, '<model_name>/information'), methods=['GET', 'POST'])
