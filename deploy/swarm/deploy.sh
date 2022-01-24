1740bec53d2c3d793798f0fa641ce55878dbb81f
Andoni Angulo <andoni.angulo@zylk.com>
2022-01-24 19:22:24 +0100

    add create network for swarm

--- deploy/swarm/deploy.sh
+++ deploy/swarm/deploy.sh
@@ -23,12 +23,12 @@ while true; do
   fi
 done
 
-# start deploying and exit on any error
 echo "Creating deploy environment with $num Modelhost replicas..."
 
-# Run
+# create network if it is not already created
+docker network create -d overlay --subnet 10.0.13.0/24 mlbuffet_overlay
+
+# start deployment and exit on any error
 docker stack deploy -c swarm.yaml mlbuffet &&
-
 sleep 5
-
 docker service update modelhost --replicas=$num
