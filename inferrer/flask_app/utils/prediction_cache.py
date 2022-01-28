9829beb2b65c17b22385b0a58c934e659ec7df76
[zylk] Sergio Martín <sergio.martin@zylk.net>
2022-01-28 11:31:10 +0100

    Cache-Redis

--- inferrer/flask_app/utils/prediction_cache.py
+++ inferrer/flask_app/utils/prediction_cache.py
@@ -1,9 +1,10 @@
 import hashlib
 import json
+import redis
 
-# TODO: This can be REDIS or SQLite
-
-default_cache_filename = '/.cache/inferrer-cache.json'
+redis_host = 'cache'
+redis_port = 6379
+redis_cli = redis.Redis(host=redis_host, port=redis_port)
 
 
 def get_hash(model_name, inputs):
@@ -12,22 +13,20 @@ def get_hash(model_name, inputs):
     return hashlib.blake2b(hash_func_input).hexdigest()
 
 
-# Read prediction from json
-def get_prediction(hash_code, filename=default_cache_filename):
-    with open(filename, 'r') as file:
-        cache_dict = json.load(file)
-        return cache_dict.get(hash_code)  # return None if not present
+def get_prediction(hash_code):
+    try:
+        result = redis_cli.get(hash_code)
+        if result is not None:
+            result = json.loads(result)
+    except:
+        result = None
+    return result
 
 
 # Write prediction as json format
-def put_prediction_in_cache(hash_code, prediction, filename=default_cache_filename):
-    with open(filename, 'r+') as file:
-        # Load existing data
-        cache_dict = json.load(file)
-
-        # Add new cache entry
-        cache_dict.update({hash_code: prediction})
-
-        # Save back to json
-        file.seek(0)
-        json.dump(cache_dict, file, indent=2)
+def put_prediction_in_cache(hash_code, prediction):
+    j_prediction = json.dumps(prediction)
+    try:
+        redis_cli.set(hash_code, j_prediction)
+    except:
+        pass
