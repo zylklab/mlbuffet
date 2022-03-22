671c4614a769e79e80b1a0d25cec3084c871b3c2
[zylk] Sergio Martín <sergio.martin@zylk.net>
2022-03-22 16:27:42 +0100

    Default method added to app.py

--- buffetvc/app.py
+++ buffetvc/app.py
@@ -38,5 +38,10 @@ def download(tag):
     return bvc.download_file(name=tag, version=version)
 
 
+@server.route('/default/<tag>/<new_default>', methods = ['POST'])
+def update_default(tag, new_default):
+    return bvc.update_default(name=tag, version=new_default)
+
+
 if __name__ == '__main__':
     pass
