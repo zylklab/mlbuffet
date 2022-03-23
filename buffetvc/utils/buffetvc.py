5fd6df75342657c7c1e5745c8e99f208994d6467
[zylk] Sergio Martín <sergio.martin@zylk.net>
2022-03-23 08:43:12 +0100

    New refactor to default, and add comments

--- buffetvc/utils/buffetvc.py
+++ buffetvc/utils/buffetvc.py
@@ -14,7 +14,7 @@ extern_folder = 'modelhostfiles'
 
 def save_file(file: ds.FileStorage, tag: str, file_name: str):
     model_folder = os.path.join(archivos_folder, tag)
-    # Creation of the name folder to the model and the history/latest files
+    # Creation of the name folder to the model and the history/default files
     if not os.path.exists(model_folder):
         os.makedirs(model_folder)
         history_file = os.path.join(model_folder, HISTORY)
@@ -26,7 +26,7 @@ def save_file(file: ds.FileStorage, tag: str, file_name: str):
         with open(latest_file, "w") as hf:
             hf.write('0')
 
-    # Check the latest file to find the version of the new file
+    # Check the default file to find the version of the new file
     with open(os.path.join(model_folder, DEFAULT), 'r') as fl:
         last_folder = int(fl.read())
         new_folder = str(last_folder + 1)
@@ -50,7 +50,7 @@ def save_file(file: ds.FileStorage, tag: str, file_name: str):
         fh.write(json.dumps(data, sort_keys=True))
         fh.close()
 
-    # Rewrite the latest file with the new version
+    # Rewrite the default file with the new version
     with open(os.path.join(model_folder, DEFAULT), 'w') as fl:
         fl.write(new_folder)
 
@@ -64,14 +64,13 @@ def save_file(file: ds.FileStorage, tag: str, file_name: str):
     if len(os.listdir(extern_model_folder)) != 0:
         for file_to_remove in os.listdir(extern_model_folder):
             filer = os.path.join(extern_model_folder, file_to_remove)
-            print(filer)
             os.remove(filer)
 
     shutil.copy(intern_path, extern_path)
 
 
 def remove_file(name: str, version: str):
-    latest_file = os.path.join(archivos_folder, name, DEFAULT)
+    default_file = os.path.join(archivos_folder, name, DEFAULT)
     history_file = os.path.join(archivos_folder, name, HISTORY)
 
     folders = []
@@ -90,7 +89,7 @@ def remove_file(name: str, version: str):
 
     # Check if the folder will be empty
     try:
-        folders[-2]
+        new_default_file = folders[-2]
     except IndexError:
         no_files = True
 
@@ -101,23 +100,19 @@ def remove_file(name: str, version: str):
         extern_tagged_folder = os.path.join(extern_folder, name)
         shutil.rmtree(extern_tagged_folder)
     else:
-        # Open the latest file and checks the version of the file
-        with open(latest_file, 'r') as lf:
-            data_latest = lf.read()
-            if data_latest == version or version == 'default':
-                version = data_latest
+        # Open the default file and checks the version of the file
+        with open(default_file, 'r') as lf:
+            data_default = lf.read()
+            if data_default == version or version == 'default':
+                version = data_default
             lf.close()
 
-        # Rewrite the latest file with the last version available
-        with open(latest_file, 'w') as lf:
-            if int(data_latest) == int(version):
-                try:
-                    new_last_file = folders[-2]
-                    lf.write(str(new_last_file))
-                except IndexError:
-                    lf.write('')
+        # Rewrite the default file with the last version available
+        with open(default_file, 'w') as lf:
+            if int(data_default) == int(version):
+                lf.write(str(new_default_file))
             else:
-                lf.write(data_latest)
+                lf.write(data_default)
             lf.close()
 
         # Rewrite the history file without the information of the removed file
@@ -129,11 +124,11 @@ def remove_file(name: str, version: str):
         folder_file = os.path.join(archivos_folder, name, str(version))
         shutil.rmtree(folder_file)
 
-        # Put the last versioned file in the extern folder
-        with open(latest_file, 'r') as lf:
-            data_latest = lf.read()
-            print(data_latest)
-            new_last_path = os.path.join(archivos_folder, name, str(data_latest))
+        # Put the default versioned file in the extern folder
+        with open(default_file, 'r') as lf:
+            data_default = lf.read()
+            print(data_default)
+            new_default_path = os.path.join(archivos_folder, name, str(data_default))
         extern_path = os.path.join(extern_folder, name)
 
         # Check if the extern_path is empty. If not, remove the file
@@ -142,13 +137,13 @@ def remove_file(name: str, version: str):
             for file_extern in files_extern_path:
                 os.remove(os.path.join(extern_path, file_extern))
 
-        shutil.copytree(new_last_path, extern_path, dirs_exist_ok=True)
+        shutil.copytree(new_default_path, extern_path, dirs_exist_ok=True)
 
 
 def download_file(name: str, version: str):
     # Check default file
     if version == 'default':
-        with open(os.path.join(archivos_folder, name, '.latest'), 'r') as lf:
+        with open(os.path.join(archivos_folder, name, DEFAULT), 'r') as lf:
             version = lf.read()
             lf.close()
     folder_path = os.path.join(archivos_folder, name, version)
@@ -165,23 +160,30 @@ def download_file(name: str, version: str):
 def update_default(name: str, version: str):
     folder_path = os.path.join(archivos_folder, name)
     extern_folder_path = os.path.join(extern_folder, name)
-    try:
 
-        with open(os.path.join(folder_path, '.latest'), 'w') as lf:
+    try:
+        # Rewrite .default file with the new default tag
+        with open(os.path.join(folder_path, DEFAULT), 'w') as lf:
             lf.write(version)
             lf.close()
-            file_to_remove = os.listdir(os.path.join(extern_folder, name))[0]
-            os.remove(os.path.join(extern_folder_path, file_to_remove))
-            new_default_file = os.listdir(os.path.join(folder_path, version))[0]
-            path_to_default = os.path.join(extern_folder_path, new_default_file)
-            shutil.copy(new_default_file, path_to_default)
-            return Response(f'The file {name} with the version {version} has been set as default\n')
+
+        # Remove the file in the external path
+        file_to_remove = os.listdir(os.path.join(extern_folder, name))[0]
+        os.remove(os.path.join(extern_folder_path, file_to_remove))
+
+        # Copy the new default file into the external path
+        new_default_file = os.listdir(os.path.join(folder_path, version))[0]
+        path_to_default = os.path.join(extern_folder_path, new_default_file)
+        shutil.copy(new_default_file, path_to_default)
+        return Response(f'The file {name} with the version {version} has been set as default\n')
     except FileNotFoundError:
         return Response('File not found, please check the name introduced\n')
 
 
 def get_information(name: str):
     folder_path = os.path.join(archivos_folder, name)
+
+    # Read the history file
     try:
         with open(os.path.join(folder_path, HISTORY), 'r') as hf:
             data = hf.read()
