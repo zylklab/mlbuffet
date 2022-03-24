c4a491a98ad87a1b41209a40e7ac6c57a188b7dd
agg001 <agonzalezgil@zylk.net>
2022-03-24 11:53:16 +0100

    Added open() mehotd to send file instead of plain text

--- inferrer/flask_app/utils/find.py
+++ inferrer/flask_app/utils/find.py
@@ -5,17 +5,16 @@ from sys import argv
 
 FILENAME = argv[1]
 
+
 def get_file_extension(file_name):
     return os.path.splitext(file_name)[1]
 
-allowed_extensions = [".h5", ".onnx", ".pkl", ".pt", ".pmml", ".pb", ".zip", ".mlmodel"]
 
-print(os.path.basename(FILENAME))
-print(get_file_extension(os.path.basename(FILENAME)))
+allowed_extensions = [".h5", ".onnx", ".pkl",
+                      ".pt", ".pmml", ".pb", ".zip", ".mlmodel"]
 
 for file in glob.glob(f'/home/trainer/**/{FILENAME}', recursive=True):
     if file is not None and get_file_extension(os.path.basename(file)) in allowed_extensions:
-        
-        print(file)
-        response = requests.put(f'http://172.17.0.1:8002/api/v1/models/{os.path.basename(file)}', files={"path": file})
-        print(response.content)
+
+        response = requests.put(
+            f'http://172.17.0.1:8002/api/v1/models/{os.path.basename(file)}', files={"path": open(file, 'rb')})
