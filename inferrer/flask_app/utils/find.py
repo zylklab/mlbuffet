4434b29f5348d7474944f74357311769d7142e5f
[zylk] Sergio Martín <sergio.martin@zylk.net>
2022-03-29 08:22:10 +0200

    Upload script to use tags

--- inferrer/flask_app/utils/find.py
+++ inferrer/flask_app/utils/find.py
@@ -5,6 +5,8 @@ from sys import argv
 
 FILENAME = argv[1]
 
+TAG = argv[2]
+
 
 def get_file_extension(file_name):
     return os.path.splitext(file_name)[1]
@@ -17,4 +19,4 @@ for file in glob.glob(f'/home/trainer/**/{FILENAME}', recursive=True):
     if file is not None and get_file_extension(os.path.basename(file)) in allowed_extensions:
 
         response = requests.put(
-            f'http://172.17.0.1:8002/api/v1/models/{os.path.basename(file)}', files={"path": open(file, 'rb')})
+            f'http://172.17.0.1:8002/api/v1/models/{TAG}', files={"path": open(file, 'rb')})
