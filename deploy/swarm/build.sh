646ae5dea720259dac07a0b1566ac4507d854df4
Alfonso <agonzalezgil@zylk.net>
2022-04-04 13:22:53 +0200

    Modified to support :arm64 tag on images

--- deploy/swarm/build.sh
+++ deploy/swarm/build.sh
@@ -1,5 +1,5 @@
-docker build -t mlbuffet_inferrer  ../../inferrer/flask_app/
-docker build -t mlbuffet_modelhost ../../modelhost/flask_app/
-docker build -t mlbuffet_prometheus ../../metrics/prometheus/
-docker build -t mlbuffet_cache ../../cache/
-docker build -t mlbuffet_storage ../../storage/
+docker build -t mlbuffet_inferrer:arm64  ../../inferrer/flask_app/
+docker build -t mlbuffet_modelhost:arm64 ../../modelhost/flask_app/
+docker build -t mlbuffet_prometheus:arm64 ../../metrics/prometheus/
+docker build -t mlbuffet_cache:arm64 ../../cache/
+docker build -t mlbuffet_storage:arm64 ../../storage/
