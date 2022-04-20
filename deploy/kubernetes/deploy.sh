56ed82e2f0e9172bbb7b7c1e7a45b0c471ca16ae
Alfonso <agonzalezgil@zylk.net>
2022-04-20 09:06:15 +0200

    Refactored deploy.sh to make it simpler and include namespace

---
+++ deploy/kubernetes/deploy.sh
@@ -0,0 +1,7 @@
+kubectl apply -f ./autodeploy/mlbuffet_namespace.yaml
+kubectl apply -f ./autodeploy/volume.yaml
+kubectl apply -f ./autodeploy/modelhost.yaml
+kubectl apply -f ./autodeploy/metrics.yaml
+kubectl apply -f ./autodeploy/inferrer.yaml
+kubectl apply -f ./autodeploy/storage.yaml
+kubectl apply -f ./autodeploy/cache.yaml
