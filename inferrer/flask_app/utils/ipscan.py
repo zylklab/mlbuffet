0ca22fefabde196927a9550d45037083f2958588
[zylk] Sergio Martín <sergio.martin@zylk.net>
2022-05-09 11:17:15 +0200

    create IPScan function to kubernetes deployment

--- inferrer/flask_app/utils/ipscan.py
+++ inferrer/flask_app/utils/ipscan.py
@@ -1,5 +1,7 @@
 import nmap
+from kubernetes import client, config
 
+# IPScan for Docker Swarm
 
 def IPScan(network):
     nm = nmap.PortScanner()
@@ -17,4 +19,18 @@ def IPScan(network):
         if 'modelhost' in is_host:
             modelhostlist.append(addr)
 
+    return modelhostlist
+
+
+# IPScan for Kubernetes
+
+def KubeIPScan():
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+    modelhostlist = []
+    ret = v1.list_namespaced_pod('mlbuffet')
+    for item in ret.items:
+        if item.metadata.labels['app'] == 'mlbuffet_modelhost':
+            modelhostlist.append(item.status.pod_ip)
+        # print([item.status.pod_ip, item.metadata.name, item.metadata])
     return modelhostlist
