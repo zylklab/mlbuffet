cd37d4c0115423a63734465b61cdb70f81c28c71
Andoni Angulo <andoni.angulo@zylk.com>
2022-05-10 13:09:26 +0200

    refactor HttpJsonResponse to make it more clear

--- storage/flask_app/utils/storage_pojos.py
+++ storage/flask_app/utils/storage_pojos.py
@@ -1,4 +1,6 @@
-from flask import jsonify
+from typing import Tuple
+
+from flask import jsonify, Response
 from werkzeug.exceptions import default_exceptions
 from werkzeug.http import HTTP_STATUS_CODES
 
@@ -8,32 +10,34 @@ class HttpJsonResponse:
                  http_status_code: int,
                  http_status_name: str = None,
                  http_status_description: str = None):
-        # TODO not jic; check if not (X or Y)
-        # compute just in case values if not specified
-        if http_status_code in default_exceptions:  # if provided code belongs to a http exception
-            exception = default_exceptions[http_status_code]()
-            jic_http_status_name = exception.name
-            jic_http_status_description = exception.description
-        else:
-            jic_http_status_name = jic_http_status_description = HTTP_STATUS_CODES.get(http_status_code, '')
-
-        # if status name was not specified
-        if not http_status_name:
-            http_status_name = jic_http_status_name
-
-        # if status description was not specified
-        if not http_status_description:
-            http_status_description = jic_http_status_description
-
-        http_status = dict()
-        http_status['code'] = http_status_code
-        http_status['name'] = http_status_name
-        http_status['description'] = http_status_description
 
         self.data = dict()
+
+        # create response content
+        http_status = dict()
+        http_status['code'] = http_status_code
+
+        # if status name was specified
+        if http_status_name:
+            http_status['name'] = http_status_name
+        else:
+            if http_status_code in default_exceptions:  # if provided code belongs to a http exception
+                http_status['name'] = default_exceptions[http_status_code]().name
+            else:
+                http_status['name'] = HTTP_STATUS_CODES.get(http_status_code, '')
+
+        # if status description was specified
+        if http_status_description:
+            http_status['description'] = http_status_description
+        else:
+            if http_status_code in default_exceptions:  # if provided code belongs to a http exception
+                http_status['description'] = default_exceptions[http_status_code]().description
+            else:
+                http_status['description'] = HTTP_STATUS_CODES.get(http_status_code, '')
+
         self.data['http_status'] = http_status
 
-    def json(self):
+    def json(self) -> Tuple[Response, int]:
         return jsonify(**self.data), self.data['http_status']['code']
 
 
