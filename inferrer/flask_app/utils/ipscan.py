cb762d2c9e91e579c6f1cfb340e2bf38eca68032
[zylk] Sergio Martín <sergio.martin@zylk.net>
2022-05-10 09:11:38 +0200

    Update IPScan function to distinguish and operate whether it is on Kubernetes or Docker Swarm

--- inferrer/flask_app/utils/ipscan.py
+++ inferrer/flask_app/utils/ipscan.py
@@ -3,34 +3,44 @@ from kubernetes import client, config
 
 # IPScan for Docker Swarm
 
-def IPScan(network):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=network, arguments="-sn")
+def IPScan(service: str):
+    """
+    IPScan search the IPs of the service nodes. The way of how IPScan tries to do it, is different if MLBuffet is
+    deployed on Kubernetes or Docker Swarm.
+    """
 
     listaddr = nm.all_hosts()
     modelhostlist = []
 
-    for addr in listaddr:
-        try:
-            is_host = nm[addr].get('hostnames')[0]['name']
-        except IndexError:
-            is_host = ''
+    key = 'ORCHESTRATOR'
 
         if 'modelhost' in is_host:
             modelhostlist.append(addr)
 
-    return modelhostlist
+    if getenv(key) == 'KUBERNETES':
+        config.load_incluster_config()
+        v1 = client.CoreV1Api()
+        servicelist = []
+        service_name = 'mlbuffet_' + service
+        ret = v1.list_namespaced_pod('mlbuffet')
+        for item in ret.items:
+            if item.metadata.labels['app'] == service_name:
+                servicelist.append(item.status.pod_ip)
 
 
-# IPScan for Kubernetes
+    else:
+        network = getenv('OVERLAY_NETWORK')
+        nm = nmap.PortScanner()
+        nm.scan(hosts=network, arguments="-sn")
+        listaddr = nm.all_hosts()
+        servicelist = []
 
-def KubeIPScan():
-    config.load_incluster_config()
-    v1 = client.CoreV1Api()
-    modelhostlist = []
-    ret = v1.list_namespaced_pod('mlbuffet')
-    for item in ret.items:
-        if item.metadata.labels['app'] == 'mlbuffet_modelhost':
-            modelhostlist.append(item.status.pod_ip)
-        # print([item.status.pod_ip, item.metadata.name, item.metadata])
-    return modelhostlist
+        for addr in listaddr:
+            try:
+                is_host = nm[addr].get('hostnames')[0]['name']
+            except IndexError:
+                is_host = ''
+            if service in is_host:
+                servicelist.append(addr)
+
+    return servicelist
