f2d1925e630c32fe82aeaa788bfccf1035fb045d
alfonsozylk <agonzalezgil@zylk.net>
2022-05-10 07:57:25 +0000

    Refactoring images names.

--- deploy/kubernetes/deploy.sh
+++ deploy/kubernetes/deploy.sh
@@ -1,9 +1,3 @@
-export INFERRER=
-export MODELHOST=
-export CACHE=
-export STORAGE=
-export METRICS=
-
 kubectl apply -f ./autodeploy/mlbuffet_namespace.yaml
 kubectl apply -f ./autodeploy/volume.yaml
 kubectl apply -f ./autodeploy/rbac.yaml
