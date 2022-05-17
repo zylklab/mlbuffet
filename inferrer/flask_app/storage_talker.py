7af3ef3b0d27c1cf494ecc929f2cae3cf14f8341
AlfonsoGonz <agonzalezgil@zylk.net>
2022-05-17 19:11:02 +0200

    Removed things that are not expected to be done by storage (Storage does not have update_models())

--- inferrer/flask_app/storage_talker.py
+++ inferrer/flask_app/storage_talker.py
@@ -7,7 +7,6 @@ import requests
 from flask import send_file
 
 from utils.inferer_pojos import HttpJsonResponse
-from utils.ipscan import IPScan
 
 # Request constants
 OVERLAY_NETWORK = getenv('OVERLAY_NETWORK')
@@ -38,8 +37,6 @@ def _post(resource, json_data):
 
 def _put(resource, files):
     response = requests.put(_url(resource), files=files).json()
-    if _is_ok(response['http_status']['code']):
-        update_models()
     return response
 
 
@@ -51,7 +48,6 @@ def _delete(resource):
             update_models()
     except JSONDecodeError:
         response = raw_response.text
-        update_models()
 
     return response
 
@@ -87,7 +83,6 @@ def download_model(tag):
 def set_default_model(tag, new_version):
     resource = '/storage/model/' + tag + '/default'
     json_data = {'default': new_version}
-    update_models()
     return _post(resource, json_data)
 
 
@@ -96,33 +91,6 @@ def get_tag_information(tag):
     return _get(resource)
 
 
-def test_load_balancer(data_array):
-    resource = '/storage/api/test'
-
-    jobs = [gevent.spawn(_get, path.join(resource, str(elem)))
-            for elem in data_array]
-    gevent.wait(jobs)
-
-    # Print modelhosts responses and check if all HTTP codes are 2XX
-    all_responses_2xx = True
-    for job in jobs:
-        if not _is_ok(job.value['http_status']['code']):
-            all_responses_2xx = False
-
-        print(f'Received response: {job.value}')
-
-    if all_responses_2xx:
-        return HttpJsonResponse(200).get_response()
-    return HttpJsonResponse(
-        500, http_status_description='One or more modelhosts returned non 2XX HTTP code').get_response()
-
-
-def update_models():
-    resource = '/modelhost/updatemodels'
-
-    MODELHOST_IP_LIST = IPScan('modelhost')
-    for IP in MODELHOST_IP_LIST:
-        url = URI_SCHEME + IP + ":8000" + resource
-        print(url)
-        gevent.spawn(requests.get, url=url)
-    return HttpJsonResponse(200).get_response()
+def get_model_list():
+    resource = '/storage/models'
+    return _get(resource)
