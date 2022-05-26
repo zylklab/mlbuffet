50d9ec18f393a65195ee41a3352b80e476d7dc16
AlfonsoGonz <agonzalezgil@zylk.net>
2022-05-26 10:55:00 +0200

    Kubernetes orchestration awareness and calls to inferrer service instead of docker daemon after training

--- modules/inferrer/flask_app/utils/find.py
+++ modules/inferrer/flask_app/utils/find.py
@@ -16,5 +16,10 @@ TAG = getenv('TAG')
 
 for file in glob.glob(f'/home/trainer/**/{FILENAME}', recursive=True):
     if file is not None and get_file_extension(file) in ALLOWED_EXTENSIONS:
-        response = requests.put(
-            f'http://172.17.0.1:8002/api/v1/models/{TAG}', files={"path": open(file, 'rb')})
+
+        if getenv('ORCHESTRATOR') == 'KUBERENTES':
+            response = requests.put(
+                f'http://inferrer:8000/api/v1/models/{TAG}', files={"path": open(file, 'rb')})
+        else:
+            response = requests.put(
+                f'http://172.17.0.1:8002/api/v1/models/{TAG}', files={"path": open(file, 'rb')})
