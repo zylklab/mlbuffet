0e3be7ad548fd5f68037764b3a38467929d3a2fd
AlfonsoGonz <agonzalezgil@zylk.net>
2022-06-02 17:20:06 +0200

    Check if ML_LIBRARY is set at Pod creation

--- modules/modelhost/flask_app/modify_requirements.sh
+++ modules/modelhost/flask_app/modify_requirements.sh
@@ -1,2 +1,7 @@
-if $ML_LIBRARY NOT NULL¿?¿?
-    pip install $ML_LIBRARY
+# Check if ML_LIBRARY is set. If not, pass.
+if [${ML_LIBRARY-""} == ""]
+then
+        echo 'No Model Library specified, installing latest version of detected library...'
+else
+        pip install -y $ML_LIBRARY
+fi
