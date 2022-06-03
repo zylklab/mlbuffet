a4e7f95ef3d88d774c750605a0c560ad28a0dff8
AlfonsoGonz <agonzalezgil@zylk.net>
2022-06-03 14:10:29 +0200

    Refactored and corrected modify_requirements into install_ml_libraries

--- modules/modelhost/flask_app/modify_requirements.sh
+++
@@ -1,7 +0,0 @@
-# Check if ML_LIBRARY is set. If not, pass.
-if [${ML_LIBRARY-""} == ""]
-then
-        echo 'No Model Library specified, installing latest version of detected library...'
-else
-        pip install -y $ML_LIBRARY
-fi
