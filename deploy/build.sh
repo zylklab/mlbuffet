9d95139160f6d2c3d27e26fc3d6209303df06965
AlfonsoGonz <agonzalezgil@zylk.net>
2022-06-06 10:21:31 +0200

    Corrected image names to be generic

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,6 +1,6 @@
-docker build -t localhost:5000/mlbuffet_inferrer  ../modules/inferrer/flask_app/
-docker build -t localhost:5000/mlbuffet_modelhost ../modules/modelhost/flask_app/
-docker build -t localhost:5000/mlbuffet_metrics ../modules/metrics/prometheus/
-docker build -t localhost:5000/mlbuffet_cache ../modules/cache/
-docker build -t localhost:5000/mlbuffet_storage ../modules/storage/flask_app/
-docker build -t localhost:5000/mlbuffet_trainer ../modules/trainer/
+docker build -t mlbuffet_inferrer  ../modules/inferrer/flask_app/
+docker build -t mlbuffet_modelhost ../modules/modelhost/flask_app/
+docker build -t mlbuffet_metrics ../modules/metrics/prometheus/
+docker build -t mlbuffet_cache ../modules/cache/
+docker build -t mlbuffet_storage ../modules/storage/flask_app/
+docker build -t mlbuffet_trainer ../modules/trainer/
