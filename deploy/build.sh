69d5e3ed076b161cabe5c2656dbc15aeb82d05f4
zylk <zylk@kubrick000.bigdata.zylk.net>
2022-06-06 08:18:48 +0000

    Move build.sh to deploy/ directory.

---
+++ deploy/build.sh
@@ -0,0 +1,6 @@
+docker build -t localhost:5000/mlbuffet_inferrer  ../../modules/inferrer/flask_app/
+docker build -t localhost:5000/mlbuffet_modelhost ../../modules/modelhost/flask_app/
+docker build -t localhost:5000/mlbuffet_metrics ../../modules/metrics/prometheus/
+docker build -t localhost:5000/mlbuffet_cache ../../modules/cache/
+docker build -t localhost:5000/mlbuffet_storage ../../modules/storage/flask_app/
+docker build -t localhost:5000/mlbuffet_trainer ../../modules/trainer/
