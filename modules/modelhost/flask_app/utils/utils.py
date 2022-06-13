3cfc2ca7b6bd116d3e6a8b5e5fe28d96a3e9f38d
sergiomhmh <sergiomhmh@gmail.com>
2022-06-13 15:00:54 +0200

    Refactor function of getting the ml_library

--- modules/modelhost/flask_app/utils/utils.py
+++ modules/modelhost/flask_app/utils/utils.py
@@ -1,5 +1,8 @@
 from os import path, getcwd
 import shutil
+import requests
+
+URI_SCHEME = 'http://'
 
 
 def unzip_models(file_name):
@@ -7,21 +10,20 @@ def unzip_models(file_name):
     return 'Unzipped'
 
 
+def _url(resource):
+    return URI_SCHEME + 'storage:8000' + resource
+
+
+def _get(resource):
+    return requests.get(_url(resource)).json()
+
+
+def get_tag_library(tag):
+    resource = '/storage/models/' + tag + '/library'
+    return _get(resource)
+
+
 def get_model_library(file_name):
 
-    # Check filename extension
-    extension = path.splitext(file_name)[1]
-
-    if extension == '.onnx':
-        ML_LIBRARY = 'onnx'
-    elif extension == '.pb' or '.h5' or '.zip':
-        ML_LIBRARY = 'tf'
-
-        if extension == '.zip':
-            unzip_models(file_name)
-
-    # elif add new supported libraries
-    else:
-        ML_LIBRARY = 'UNSUPPORTED'
-
-    return ML_LIBRARY
+    resource = '/storage/models/' + file_name + '/library'
+    return _get(resource)['ml_library']
