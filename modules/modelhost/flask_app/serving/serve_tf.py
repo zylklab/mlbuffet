e018adac779f54abfef32b0c0ccde170155618bb
Andoni Angulo <andoni.angulo@zylk.com>
2022-06-13 10:12:33 +0200

    Tensorflow serving: Fixed inference on h5 models, cleaned code, documented

--- modules/modelhost/flask_app/serving/serve_tf.py
+++ modules/modelhost/flask_app/serving/serve_tf.py
@@ -1,13 +1,15 @@
 import tensorflow
-import numpy
-from modules.modelhost.flask_app.utils import utils
+import shutil
 import os
-
+import numpy
 # List with preloaded models to do the inference
 model_sessions = {}
 
 
-def pb_load(tag, filename, model_name):  # tf model loading when .pb format is detected
+def _pb_load(tag, filename, model_name): # tf model loading when .pb format is detected
+    """
+    Auxiliary method for loading TF model when .pb format is detected
+    """
     tfmodel = tensorflow.saved_model.load(filename)
     inferlayer = tfmodel.signatures[
         "serving_default"]  # layer where inference happens passing the inputs as a tf.constant(inputs)
@@ -17,7 +19,7 @@ def pb_load(tag, filename, model_name):  # tf model loading when .pb format is d
     label_name = list(inferlayer.structured_outputs.keys())[0]
 
     full_description = {'tag': tag,
-                        'model_type': 'pb',  # check for inference tasks
+                        'model_type': 'pb', # check for inference tasks
                         'model_name': model_name,
                         'infer_layer': inferlayer,
                         'input_name': input_name,
@@ -28,7 +30,10 @@ def pb_load(tag, filename, model_name):  # tf model loading when .pb format is d
     return full_description
 
 
-def keras_load(tag, filename):
+def _keras_load(tag, filename):
+    """
+        Auxiliary method for loading TF model when .pb format is detected
+    """
     model = tensorflow.keras.models.load_model(filename)
     input_name = model.input_names
     output_name = model.output_names
@@ -36,7 +41,7 @@ def keras_load(tag, filename):
     label_name = model.output_names
 
     full_description = {'tag': tag,
-                        'model_type': 'h5',  # check for inference tasks
+                        'model_type': 'h5', # check for inference tasks
                         'model_object': model,
                         'model_name': filename,
                         'input_name': input_name,
@@ -48,16 +53,20 @@ def keras_load(tag, filename):
 
 
 def load_model(tag, filename):
-
-    if '.zip' in filename:  # check if zip
-        utils.unzip_models(filename)
+    """
+    Method for loading a model
+    :param tag: a tag to identify the model
+    :param filename: the stored model file. Either a .zip with the folder, a .pb or a.h5 file
+    """
+    if '.zip' in filename:  # check if zip -> unpack in a folder named with the tag
+        shutil.unpack_archive(filename, os.getcwd())
 
     files = os.listdir(os.getcwd())
     if any(File.endswith(".pb") for File in files):
         mfile = [f for f in os.listdir(os.getcwd()) if f.endswith('.pb')][0]
         try:
             # when loading a pb model, using os.getcwd as the directory param required by the load function
-            model_sessions[tag] = pb_load(tag, os.getcwd(), mfile)
+            model_sessions[tag] = _pb_load(tag, os.getcwd(), mfile)
             return model_sessions[tag]
         except Exception as e:
             print(e)
@@ -65,7 +74,7 @@ def load_model(tag, filename):
     else:  # .h5 model loaded -> keras library
         mfile = [f for f in os.listdir(os.getcwd()) if f.endswith('.h5')][0]
         try:
-            model_sessions[tag] = keras_load(tag, mfile)
+            model_sessions[tag] = _keras_load(tag, mfile)
             return model_sessions[tag]
         except Exception as e:
             print(e)
@@ -81,8 +90,13 @@ def check_model_exists(tag):
 
 
 def perform_inference(tag, model_input):
-
+    """
+    Method to perform inference on a loaded model.
+    :param tag: Model identifier
+    :param model_input: Input to be given to the model to perform the inference
+    """
     if model_sessions[tag]['model_type'] == 'h5':
+        model_input = numpy.expand_dims(numpy.asarray(model_input), axis=0)
         return model_sessions[tag]['model_object'].predict(model_input)
 
     else:
