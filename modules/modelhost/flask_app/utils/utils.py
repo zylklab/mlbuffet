b803e5603887395437eb92b5af520d3dbec43e19
agg001 <agonzalezgil@zylk.net>
2022-06-15 13:45:37 +0200

    Removed unused requests that are not done from Python.

--- modules/modelhost/flask_app/utils/utils.py
+++ modules/modelhost/flask_app/utils/utils.py
@@ -1,26 +1,7 @@
-from os import path
-import shutil
-import requests
 from zipfile import ZipFile
-
-
-URI_SCHEME = 'http://'
 
 
 def unzip_models(file_name):
     zip_ = ZipFile(file_name)
     zip_.extractall()
     return zip_.namelist()
-
-
-def _url(resource):
-    return URI_SCHEME + 'storage:8000' + resource
-
-
-def _get(resource):
-    return requests.get(_url(resource)).json()
-
-
-def get_model_library(tag):
-    resource = '/storage/models/' + tag + '/library'
-    return _get(resource)['ml_library']
